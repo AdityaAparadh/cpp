820c78dbfb67b81d12397f2dfe84a6a28ccc3b19
AdityaAparadh <aditya.aparadh.0@gmail.com>
2022-03-02 10:02:16 +0530

    cleaned up some things

--- codes/collatz.cpp
+++ codes/collatz.cpp
@@ -15,6 +15,13 @@ hide             (optional, type "hide" to hide the graph if output is messy, el
 
 
 
+- Best viewed on large screens
+
+- The values in graph have been rounded to the nearest 0.1 units
+
+- It may say "No output" for very large seeds.
+
+- If you run this code on your own compiler, make sure you give some random string (if not "hide") as input on second line, or you won't get any output
 
 
 
@@ -31,129 +38,154 @@ More info here-
 https://en.wikipedia.org/wiki/Collatz_conjecture
 
 
-The values in graph have been rounded to the nearest 0.1 units
-It may provide "No output" for very large seeds.
 
 */
 
-
-
-
-
-
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int numberOfDigits(int i)
 {
-    if (i==0){
+    if (i == 0)
+    {
         return 1;
-    }else{
-    int x = log10(i) + 1;
-    return x;
+    }
+    else
+    {
+        int x = log10(i) + 1;
+        return x;
     }
 }
 
-double approx(double x){
+double approx(double x)
+{
 
     int integralPart = x;
     double decimalPart = x - integralPart;
 
-    if(decimalPart >= 0.5){
-        return (integralPart+1);
-    }else {
+    if (decimalPart >= 0.5)
+    {
+        return (integralPart + 1);
+    }
+    else
+    {
         return (integralPart);
     }
 }
 
-double modulo(double x, double y){
+double modulo(double x, double y)
+{
 
-
-    while(x>=y){
-        x = x-y;
+    while (x >= y)
+    {
+        x = x - y;
     }
-return x;
+    return x;
 }
 
+void collatz(long double seed, string graphSwitch = "x")
+{
 
-void collatz(long double seed, string graphSwitch = "x"){
-
-if(seed<=0){
-    cout << "Please enter a postive integral value for seed." << endl << endl;
-    cout << "Current seed: " << seed  << endl;
-}else{
-
-seed = seed*2;
-int a=1;
-long double b=seed;
-
-while (b!=1){
-    if(modulo(b,2) == 0){
-        b = b/2;
-    }else{
-        b = 3*b + 1;
+    if (seed <= 0)
+    {
+        cout << "Please enter a postive integral value for seed." << endl
+             << endl;
+        cout << "Current seed: " << seed << endl;
     }
-    a++;
+    else
+    {
+
+        seed = seed * 2;
+        int a = 1;
+        long double b = seed;
+
+        while (b != 1)
+        {
+            if (modulo(b, 2) == 0)
+            {
+                b = b / 2;
+            }
+            else
+            {
+                b = 3 * b + 1;
+            }
+            a++;
+        }
+        cout << "Seed : " << seed / 2 << endl;
+        cout << "Number of iterations: " << a - 2 << "\n\n\n";
+        cout << "Numbers (n)";
+        if (graphSwitch != "hide")
+        {
+            cout << "                  Plot of natural log(n)";
+        }
+        cout << endl
+             << endl
+             << endl;
+
+        while (seed != 1)
+        {
+
+            if (modulo(seed, 2) == 0)
+            {
+                seed = seed / 2;
+            }
+            else
+            {
+                seed = 3 * seed + 1;
+            }
+            cout << seed;
+
+            if (graphSwitch != "hide")
+            {
+                int spacer = 0;
+                while ((20 - spacer) >= numberOfDigits(seed))
+                {
+                    cout << " ";
+                    spacer++;
+                }
+                cout << "|";
+                double g = approx((log(seed)) * 10);
+                int counter = 0;
+                while (counter < g)
+                {
+                    cout << "=";
+                    counter++;
+                }
+            }
+
+            cout << endl;
+        }
+        cout << "(Goes to 4)" << endl
+             << endl
+             << endl;
+
+        if (graphSwitch == "hide")
+        {
+            cout << "GRAPH HIDDEN " << endl
+                 << "To show graph, don't enter \"hide\" on the second line of input, leave it blank" << endl;
+        }
+        else
+        {
+            cout << "  SCALE: \"=\" = 0.1 units  ";
+        }
     }
-cout << "Seed : " << seed/2 << endl;
-cout << "Number of steps: " << a << "\n\n\n";
-cout << "Numbers (n)";
-if(graphSwitch!= "hide"){
-    cout << "                  Plot of natural log(n)";
 }
-cout << endl << endl << endl;
 
+int main()
+{
 
-while (seed!=1){
+    long double input;
+    string graph;
+    cin >> input >> graph;
+    collatz(input, graph);
 
-    if(modulo (seed,2) == 0){
-        seed = seed/2;
-    }else{
-        seed = 3*seed + 1;
-    }
-    cout << seed;
-
-    if (graphSwitch!="hide"){
-    int spacer = 0;
-    while((20-spacer)>=numberOfDigits(seed)){
-            cout << " ";
-            spacer++;
-    }
-    cout << "|";
-    double g= approx((log(seed))*10);
-    int counter = 0;
-    while(counter < g){
-        cout << "=";
-        counter++;
-    }
-    }
-
-    cout << endl;
-    }
-    cout<< "(Goes to 4)" << endl << endl << endl;
-
-    if(graphSwitch == "hide"){
-        cout << "GRAPH HIDDEN " << endl << "To show graph, don't enter \"hide\" on the second line of input, leave it blank" << endl;
-    }else{
-        cout << "  SCALE: \"=\" = 0.1 units  ";
-    }
-}
-}
-
-int main(){
-
-long double input;
-string graph ;
-cin >> input >> graph;
-collatz(input, graph);
-
-cout << "\n\n\n\n";
-cout << "######################################################### \n";
-cout << "##                      Made By :                      ## \n";
-cout << "##                                                     ## \n";
-cout << "##                      ADITYA                         ## \n";
-cout << "##                                                     ## \n";
-cout << "######################################################### \n";
-return 0;
+    cout << "\n\n\n\n";
+    cout << "######################################################### \n";
+    cout << "##                      Made By :                      ## \n";
+    cout << "##                                                     ## \n";
+    cout << "##                      ADITYA                         ## \n";
+    cout << "##                                                     ## \n";
+    cout << "######################################################### \n";
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 
+class object{
+
+private:
+    string name="0";
+    double mass=0;
+    double position[3] = {0,0,0};
+    double xCoordinate = position[0];
+    double yCoordinate = position[1];
+    double zCoordinate = position[2];
+    
+public:
+    object(string conName,double conMass,double conX,double conY,double conZ){
+        name=conName;
+        mass=conMass;
+        xCoordinate=conX;
+        yCoordinate=conY;
+        zCoordinate=conZ;
+    }
+    double provideX(){
+        return xCoordinate;
+    }
+    double provideY(){
+        return yCoordinate;
+    }
+    double provideZ(){
+        return zCoordinate;
+    }
+
+};
+
+
+
 int main(){
-    one();
-    cout << rand();
+
+    object Earth("terra",10000, 1,2,3);
+
+    cout << Earth.provideZ() <<endl;
     return 0;
 }   
